1d50303c137643b0897df569a40d1b90583f6f81
Zebdon <cyntiazebaze@gmail.com>
2026-03-01 20:51:58 +0100

    Corregir estilos responsive y ordenar media queries

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const toggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
+const toggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".nav-links");
 
-toggle.addEventListener('click', () => {
-    navLinks.classList.toggle('is-open');
-    const isOpen = navLinks.classList.contains('is-open');
-    toggle.setAttribute('aria-expanded', isOpen);
+toggle.addEventListener("click", () => {
+  links.classList.toggle("is-open");
+
+  const open = links.classList.contains("is-open");
+  toggle.setAttribute("aria-expanded", open ? "true" : "false");
 });
